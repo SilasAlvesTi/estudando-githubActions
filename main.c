ff3254830dda76eb70259717e1df266b7b170964
SilasAlvesTi <silas.ifce.ti@gmail.com>
2023-09-04 13:42:20 -0300

    Adicionando arquivo para testes usando python

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+int add(int a, int b) {
+    return a + b;
+}
+
 int main() {
-   printf("Hello, World!");
-   return 0;
+    int num1, num2, result;
+
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+
+    result = add(num1, num2);
+
+    printf("%d", result);
+
+    return 0;
 }
---
+++ test_code.py
@@ -0,0 +1,39 @@
+import subprocess
+
+def test_addition1():
+    input_data = "2 3/n"
+
+    result = subprocess.run(
+        "./main",
+        input=input_data.encode(),
+        stdout=subprocess.PIPE,    # Capture a saída padrão
+        stderr=subprocess.PIPE,    # Capture a saída de erro
+    )
+
+    assert int(result.stdout.decode()) == 5
+
+
+def test_addition2():
+    input_data = "4 3/n"
+
+    result = subprocess.run(
+        "./main",
+        input=input_data.encode(),
+        stdout=subprocess.PIPE,    # Capture a saída padrão
+        stderr=subprocess.PIPE,    # Capture a saída de erro
+    )
+
+    assert int(result.stdout.decode()) == 6
+
+
+def test_addition3():
+    input_data = "4 3/n"
+
+    result = subprocess.run(
+        "./main",
+        input=input_data.encode(),
+        stdout=subprocess.PIPE,    # Capture a saída padrão
+        stderr=subprocess.PIPE,    # Capture a saída de erro
+    )
+
+    assert int(result.stdout.decode()) == 7
